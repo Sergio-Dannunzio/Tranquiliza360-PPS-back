8afbf2928557a8f1fa5a5be88692ab740a728a3d
Héctor Bruña <hectorbct.2002@gmail.com>
2024-11-19 15:01:00 -0300

    get de los post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,4 @@
-const Post = require('../models/Post');
+const Post = require("../models/Post");
 
 // Crear un nuevo post
 const createPost = async (req, res) => {
@@ -13,18 +13,31 @@ const createPost = async (req, res) => {
     res.status(201).json(newPost);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Error en el servidor');
+    res.status(500).send("Error en el servidor");
   }
 };
 
 // Obtener todos los posts
 const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find().populate('user', 'username email');
-    res.json(posts);
+    const posts = await Post.find();
+    return res.json(posts);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Error en el servidor');
+    return res.status(500).send("Error en el servidor");
+  }
+};
+
+const getPostsbyTitle = async (req, res) => {
+  const { title } = req.params; // Obtiene el título de los parámetros de ruta
+  try {
+    const post = await Post.findOne({ title });
+    if (!post) {
+      return res.status(404).json({ message: "Post no encontrado" });
+    }
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
 
@@ -33,23 +46,24 @@ const deletePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post) {
-      return res.status(404).json({ msg: 'Post no encontrado' });
+      return res.status(404).json({ msg: "Post no encontrado" });
     }
 
     if (post.user.toString() !== req.user.id) {
-      return res.status(403).json({ msg: 'No autorizado' });
+      return res.status(403).json({ msg: "No autorizado" });
     }
 
     await post.remove();
-    res.json({ msg: 'Post eliminado' });
+    res.json({ msg: "Post eliminado" });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Error en el servidor');
+    res.status(500).send("Error en el servidor");
   }
 };
 
 module.exports = {
+  getPostsbyTitle,
   createPost,
   getPosts,
-  deletePost
+  deletePost,
 };
